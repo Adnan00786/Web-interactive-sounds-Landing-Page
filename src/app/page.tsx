2b0b0736b81444c209a5e3c2527fb5556aa94a18
Adnan <adnanali23189@gmail.com>
2024-06-14 18:43:50 +0530

    Major Intial commits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <Hero/>
     </>
   );
 }
